b710b3e13bc6a50a82396162b6cafd81d4c3155a
Davi Cajucaru <davicajucaru@hotmail.com>
2021-04-06 21:28:56 -0300

    db pronto

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -10,13 +10,14 @@ module.exports = {
     const jobs = Job.get();
     const lastId = jobs[jobs.length - 1]?.id || 0;
 
-    jobs.push({
+    Job.create({
       id: lastId + 1,
       name: req.body.name,
       "daily-hours": req.body["daily-hours"],
       "total-hours": req.body["total-hours"],
       createdAt: Date.now(), // atribuindo data de hoje
     });
+
     return res.redirect("/");
   },
   show(req, res) {
